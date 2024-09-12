aec665164d1c0c3599f03d8c24914ce2f0c63735
Eric Schmidt <eschmidt@csshealth.com>
2024-09-12 17:19:43 -0400

    Harpoon2 working

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,5 +3,25 @@ return {
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
         dependencies = { "nvim-lua/plenary.nvim" },
-    } 
+
+        config = function()
+            require('harpoon').setup({})
+
+            local harpoon = require("harpoon")
+
+            harpoon:setup()
+
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<C-j>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+            vim.keymap.set("n", "<C-u>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-i>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-o>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<C-p>", function() harpoon:list():select(4) end)
+
+            -- Toggle previous & next buffers stored within Harpoon list
+            vim.keymap.set("n", "<C-l>", function() harpoon:list():prev() end)
+            vim.keymap.set("n", "<C-k>", function() harpoon:list():next() end)
+        end
+    }
 }
